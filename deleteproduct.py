998d4ad54fc0ff9f91c9bb91de49f0030417b4a5
sridharandharani <sridharandharani1815@gmail.com>
2022-03-10 09:33:26 +0530

    changes done

--- deleteproduct.py
+++ deleteproduct.py
@@ -1,5 +1,5 @@
 import sqlite3
 folder = sqlite3.connect("product.db")
 getcode = input("enter the id :")
-folder.execute("DELETE * FROM PRODUCTDATA WHERE PRODUCTCODE="+getcode)
+folder.execute("DELETE FROM PRODUCTDATA WHERE PRODUCTCODE="+getcode)
 folder.commit()
